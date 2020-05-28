d97b9d07f85c619713364befeb92f89e5991bd97
Robin Venneman <robin.venneman@gmail.com>
2020-05-28 19:49:01 +0200

    add initial readme & rules script for mongo_id

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Auth0-rules
-Scripts needed to configure integration with Auth0
+# Auth0 files for FightPandemics
+
+
+## Rules 
+Scripts needed to configure integration with Auth0, they need to be activated in order for the authentication to work
+The code has been updated to work with Node 12 enabled as environment
+
+### mongo_id.js
+Set Mongo id in app metadata for verified users
+The `FP_DOMAIN` should be set in the rules configuration to the same value as used in the backend `.env AUTH_APP_URL`
+This `mongo_id` is used as primary key `_id` for users in our mongo database, and stored in the JWT
---
+++ rules/mongo_id.js
@@ -0,0 +1,33 @@
+function setMongoIdToUser(user, context, callback) {
+  // The Mongo id should only be set to verified users.
+  if (!user.email || !user.email_verified) {
+    return callback(null, user, context);
+  }
+
+  if (!user.app_metadata) {
+    user.app_metadata = {};
+  }
+
+  const path = require('path');
+  const { FP_DOMAIN } = configuration;
+  const MONGO_KEY = path.join(FP_DOMAIN, 'mongo_id');
+
+  // If the mongo_id is already set we can also skip this
+  if (user.app_metadata.mongo_id) {
+    context.accessToken[MONGO_KEY] = user.app_metadata.mongo_id;
+    return callback(null, user, context);
+  }
+
+  const bson = require('bson');
+  user.app_metadata.mongo_id = new bson.ObjectId();
+
+  auth0.users.updateAppMetadata(user.user_id, user.app_metadata)
+    .then(function () {
+      context.accessToken[MONGO_KEY] = user.app_metadata.mongo_id;
+      callback(null, user, context);
+    })
+    .catch(function (err) {
+      callback(err);
+    });
+
+}
